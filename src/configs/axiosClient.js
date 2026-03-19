77128898992a8aabc9adb7fec79a007f03781795
Pham Phuong Anh <phamphuonganh5112002@gmail.com>
2026-03-19 16:01:08 +0700

    feat: update axios baseURL to use environment variable

--- src/configs/axiosClient.js
+++ src/configs/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8888/',
+  baseURL: import.meta.env.VITE_API_BACKEND_URL || 'http://localhost:8888',
   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' },
 })
